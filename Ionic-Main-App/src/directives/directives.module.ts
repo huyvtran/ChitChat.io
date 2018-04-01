e653947ed938bd4ad945b896c713012bb379498d
Liam Jameson <ljameson@iastate.edu>
2018-04-01 16:24:05 -0500

    Deleted master for branch wipe and up-to-date code

--- Ionic-Main-App/src/directives/directives.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ParallaxHeaderDirective } from './parallax/parallax';
-@NgModule({
-	declarations: [ParallaxHeaderDirective],
-	imports: [],
-	exports: [ParallaxHeaderDirective]
-})
-export class DirectivesModule {}
